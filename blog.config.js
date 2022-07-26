d81c41326a90adb51ba0d5dafaa7a34aa75f55c8
羲奕 <100599604+Taya163@users.noreply.github.com>
2022-07-26 10:48:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '597f6ee8-4e02-42df-bc3f-75927fe53879', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
